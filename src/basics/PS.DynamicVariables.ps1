4fa8660919ec271957983381452ebe68220fd809
Linguist-Codifier <myselfcontact.jonathan@gmai.com>
2023-09-12 19:57:31 -0300

    adding new file for variabbles in PowerShell, as well as how to prompt them into the console.

---
+++ src/basics/PS.DynamicVariables.ps1
@@ -0,0 +1,9 @@
+﻿#PowerShell - By Jonathan P. Marinho | 12/09/2023 - 19:41
+
+$DynamiVariable = 'This is a dynamic variable';
+
+Write-Host $DynamiVariable;
+
+$DynamiVariable = 7000;
+
+Write-Host $DynamiVariable;
