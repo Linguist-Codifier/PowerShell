7277828c6db0312a0134bc3615be557bd9b5a1d9
Linguist-Codifier <myselfcontact.jonathan@gmai.com>
2023-09-12 03:48:04 -0300

    How to write comments in PowerShell?

---
+++ src/basics/PS.Comments.ps1
@@ -0,0 +1,5 @@
+﻿# Comments in PowerShell - By Jonathan P. Marinho | 12/09/2023 - 03:45AM
+
+#Writing comments in PowerShell.
+
+#This is how to write comments in PowerShell; simply add the 'Hash' symbol before anything you do want to comment about.
