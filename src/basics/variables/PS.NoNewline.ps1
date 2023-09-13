5abe6442510f21b0bb0e0b0c226b33334e0f3b7e
Linguist-Codifier <myselfcontact.jonathan@gmai.com>
2023-09-12 23:08:13 -0300

    more PowerShel

--- src/basics/variables/PS.NoNewline.ps1
+++ src/basics/variables/PS.NoNewline.ps1
@@ -1,1 +1,4 @@
-﻿
+﻿#PowerShell - By Jonathan P. Marinho | 12/09/2023 - 23:02PM
+
+Write-Host;
+Write-Host "This message is being displayed first." -NoNewline 'Now, this one is being displayed at the same line but after the first one.';
