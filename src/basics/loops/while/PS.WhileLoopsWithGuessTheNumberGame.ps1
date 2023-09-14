f389a0cd37a3eaadd76adb4d21b2752f3fb92352
Linguist-Codifier <myselfcontact.jonathan@gmai.com>
2023-09-14 15:19:17 -0300

    organizing and fixing

--- src/basics/loops/while/PS.WhileLoopsWithGuessTheNumberGame.ps1
+++ src/basics/loops/while/PS.WhileLoopsWithGuessTheNumberGame.ps1
@@ -2,27 +2,36 @@
 
 [Int32]$SecretNumber = 7;
 
-[String]$DefaultMessage = 'Guess the secret number';
-
-[String]$DefaultMessageOnError = 'Not yet'; 
+[String]$DefaultMessage = 'Guess the secret number'; [String]$DefaultMessageOnError = 'Not yet';
 
 [Boolean]$DefaultMessageWasCalled = $false;
 
-try { while(([Int32]$Buffer = Read-Host -Prompt $DefaultMessage) -ne $SecretNumber) {
-        if($DefaultMessageWasCalled -eq $false) {
+try
+{
+    $Buffer = $null;
+
+    while(([Int32]$Buffer = Read-Host -Prompt $DefaultMessage) -ne $SecretNumber)
+    {
+        if(!$DefaultMessageWasCalled)
+        {
             $DefaultMessageWasCalled = $true;
             $DefaultMessage = $DefaultMessageOnError;
         }
-  }
-      #Random comment, because this algorithm might be written in so many differents ways.
+    }
+    #Random comment, because this algorithm might be written in so many differents ways.
 }
 
-catch { Write-Host -ForegroundColor Yellow 'It seems you do not want to play the game,' -NoNewline;
-            Write-Host -ForegroundColor Yellow ' as it needs to be a number. So please try again.';
+catch
+{
+    Write-Host -ForegroundColor Yellow 'It seems you do not want to play the game,' -NoNewline;
+    Write-Host -ForegroundColor Yellow ' as it needs to be a number. So please try again.';
 }
 
-finally { if($Buffer -eq $SecretNumber) {
-            Write-Host -ForegroundColor Yellow 'Congrats, you guessed it right!';
+finally
+{
+    if($Buffer -eq $SecretNumber)
+    {
+        Write-Host -ForegroundColor Yellow 'Congrats, you guessed it right!';
     }
 }
 
