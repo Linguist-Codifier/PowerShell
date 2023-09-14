acb3bc3a44579aa2648c2c72ff6af9d1cefc4fd8
Linguist-Codifier <myselfcontact.jonathan@gmai.com>
2023-09-14 03:15:12 -0300

    working with while loops.

--- src/basics/loops/while/PS.While.ps1
+++ src/basics/loops/while/PS.While.ps1
@@ -1,1 +1,12 @@
-﻿
+#PowerShell - By Jonathan P. Marinho | 14/09/2023 - 02:00AM
+
+[String]$ConsoleContentFromInput = Read-Host -Prompt "Say us your name please";
+
+[String]$AnswerBuffer = Read-Host -prompt "Do you confirm your name is $ConsoleContentFromInput [y/n]";
+    
+    while($AnswerBuffer -ne 'y' -and $AnswerBuffer -ne 'n') {
+        [String]$AnswerBuffer = Read-Host -prompt "Do you confirm your name is $ConsoleContentFromInput [y/n]";
+    }
+
+    Write-Host -ForegroundColor Yellow 'Thanks for your support.';
+
