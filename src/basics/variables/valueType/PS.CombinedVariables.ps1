75351b8a68678a68cefbbeb8a83de5b8ca41f5d4
Linguist-Codifier <myselfcontact.jonathan@gmai.com>
2023-09-14 17:16:39 -0300

    rearranging files, organizing and adding more files about loops, and reference values in powershell, as well as intro to Functions

---
+++ src/basics/variables/valueType/PS.CombinedVariables.ps1
@@ -0,0 +1,23 @@
+﻿#PowerShell - By Jonathan P. Marinho | 12/09/2023 - 20:00PM
+
+Write-Host; #Jumping to the next line in the Console.
+
+$DynamicVariable = "This is a dynamic variable's content assigned at the same line.";
+
+Write-Host "The content of the dataholder called 'DynamicVariable' is: '$DynamicVariable'";
+
+Write-Host;
+
+$EthanMelbourneName = 'Ethan Melbourne';
+
+$EthanMelbourneWifeName = 'Mary Melbourne';
+
+$EthanMelbourneSonName = 'Isaac Melbourne';
+
+Write-Host "Random Family Names"; Write-Host;
+
+Write-host "Father name is: $EthanMelbourneName";
+
+Write-Host "Mother name is: $EthanMelbourneWifeName";
+
+Write-Host "Son name is: $EthanMelbourneSonName";
