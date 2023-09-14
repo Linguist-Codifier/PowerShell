f3e8db408096d18e034b87730f901fcab55b0a6c
Linguist-Codifier <myselfcontact.jonathan@gmai.com>
2023-09-14 17:18:43 -0300

    organizing things, adding folders, and starting t write stuff about functions, value types, references and so on...

--- src/basics/variables/PS.DynamicVariables.ps1
+++
@@ -1,8 +0,0 @@
-﻿#PowerShell - By Jonathan P. Marinho | 12/09/2023 - 22:46PM
-
-$MilisecondsCount = 1000;
-
-$SecondsCount = 1;
-
-Write-Host $MilisecondsCount;
-Write-Host $SecondsCount;
--- src/basics/variables/PS.NoNewline.ps1
+++
@@ -1,4 +0,0 @@
-﻿#PowerShell - By Jonathan P. Marinho | 12/09/2023 - 23:02PM
-
-Write-Host;
-Write-Host "This message is being displayed first." -NoNewline 'Now, this one is being displayed at the same line but after the first one.';
