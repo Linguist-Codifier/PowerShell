5bcaca897353436cc8c4db668c4af680f5bd0b26
Linguist-Codifier <myselfcontact.jonathan@gmai.com>
2023-09-14 02:52:44 -0300

    working with console default IO.

---
+++ src/basics/prompts/PS.Prompts.ps1
@@ -0,0 +1,7 @@
+﻿#PowerShell - By Jonathan P. Marinho | 14/09/2023 - 02:00AM
+
+[String]$ConsoleContentFromInput = Read-Host -Prompt "Say us your name please";
+
+[String]$AnswerBuffer = Read-Host -prompt "Do you confirm your name is $ConsoleContentFromInput";
+
+Write-Host -ForegroundColor Yellow 'Thanks for your support!';
