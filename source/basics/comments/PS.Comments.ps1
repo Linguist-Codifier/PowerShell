edae56e98bca041c42650c8600fffd5521e2cba6
Linguist-Codifier <myselfcontact.jonathan@gmail.com>
2023-09-28 19:02:26 -0300

    adding new line for code exemple

--- source/basics/comments/PS.Comments.ps1
+++ source/basics/comments/PS.Comments.ps1
@@ -3,3 +3,5 @@
 #Writing comments in PowerShell.
 
 #This is how to write comments in PowerShell; simply add the 'Hash' symbol before anything you do want to comment about.
+
+[Int32]$SQLInstructionsQuanitity = 12; # Indicartes the amount of SQL instructions.
